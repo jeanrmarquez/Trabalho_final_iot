85edc542b153d723ee78b8bed4acc46c379b2d99
Jean Robert <jeanr.marquez@gmail.com>
2022-08-14 22:03:35 -0300

    Software with gpios and dht

--- app.py
+++ app.py
@@ -1,50 +1,63 @@
 from flask import Flask, render_template
-
+import RPi.GPIO as GPIO
 from random import randint
- 
+import time
+import serial
+
+comunicacaoSerial = serial.Serial('/dev/ttyUSB0', 9600)
+
 app = Flask(__name__)
 
-state = False
+GPIO.setmode(GPIO.BOARD)
+rele = 16
+GPIO.setup(rele, GPIO.OUT)
+GPIO.output(rele, GPIO.HIGH)
 
-@app.route("/") 
+
+@app.route("/")
 def main():
-   return render_template('index.html')
- 
+    return render_template('index.html')
+
+
 @app.route("/<pin>/<action>")
 def sensor(pin, action):
-   global state
-   temperature = ''
-   humidity = ''
+    temperature = ''
+    humidity = ''
 
-   if pin == "rele" and action == "on":
-      state = True
-      print("Pin 1 is on")
-   if pin == "rele" and action == "off":
-      state = False
-      print("Pin 1 is off")
+    if pin == "rele" and action == "on":
+        GPIO.output(rele, GPIO.LOW)
+        print('on')
+    if pin == "rele" and action == "off":
+        GPIO.output(rele, GPIO.HIGH)
+        print('off')
 
+    comunicacaoSerial.flush()
+    comunicacaoSerial.flushInput()
+    comunicacaoSerial.flushOutput()
+    time.sleep(2)
 
-   temperature = randint(0, 100) 
-   humidity =  randint(0, 100)
- 
-   templateData = {
-   'temperature' : temperature,
-   'humidity' : humidity
-   }
- 
-   return render_template('index.html', **templateData)
+    dados = comunicacaoSerial.readline()
+    string_a_tratar = str(dados)
 
+    temp_and_hum = string_a_tratar.split("e")
+    umidade = temp_and_hum[0].split("b'")
+    temperatura = temp_and_hum[1].split("\\r")
 
-@app.route("/rele", methods=['GET'])
-def rele():
-   global state
-   if state == False:
-      state = True
-      print("Ligado")
-   else:
-      state = False
-      print("Desligado")
-   return True
+    umidade_float = float(umidade[1])
+    temperatura_float = float(temperatura[0])
+
+    humidity = '{0:0.1f}'.format(umidade_float)
+    temperature = '{0:0.1f}'.format(temperatura_float)
+    print(humidity)
+    print(temperature)
+
+    templateData = {
+        'temperature': temperature,
+        'humidity': humidity
+    }
+
+    return render_template('index.html', **templateData)
+
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
